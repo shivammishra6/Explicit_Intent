e286548278931f27373037b6f5613a045364db75
shivam <shivammmishra6203@gmail.com>
2024-07-20 22:32:57 +0530

    creating merge conflict

--- app/src/main/java/com/example/explicitintent/MainActivity.java
+++ app/src/main/java/com/example/explicitintent/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("hello");
+        System.out.println("shivam");
     }
     public void send(View v){
         TextView tv=findViewById(R.id.editTextText);
